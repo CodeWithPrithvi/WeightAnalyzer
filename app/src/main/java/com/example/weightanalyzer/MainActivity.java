f930d3d37f83b91229c744be75d37930885ad47d
Prithviraj <prithvirajsingh582@gmail.com>
2024-03-25 20:08:50 +0530

    added ability to add gender and amount of activity

--- app/src/main/java/com/example/weightanalyzer/MainActivity.java
+++ app/src/main/java/com/example/weightanalyzer/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.weightanalyzer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -16,6 +18,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     EditText nme,wgt;
     Button anlz;
+    Spinner activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
             nme = findViewById(R.id.name);
             wgt = findViewById(R.id.weight);
             anlz = findViewById(R.id.btn);
+            activity = findViewById(R.id.activitySelecter);
+            String[] activities={"No Form of Activity","Exercise 1-3 Times a Week","Exercise 4-6 Times a week","High Intensive Physical Work"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,activities);
+            activity.setAdapter(adapter);
             anlz.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
